b9debc3e6b0e63560794b1ed8634d87de62c739b
Gamepix10 <72648532+Gamepix10@users.noreply.github.com>
2023-02-08 15:35:33 +0300

    Add files via upload

---
+++ test_db.py
@@ -0,0 +1,52 @@
+from models import *
+import pytest
+
+
+# @pytest.fixture
+# def get_db_session():
+#    session = session()
+#    try:
+#        yield session
+#    finally:
+#        session.close()
+
+
+def test_db_data():
+    with db:
+        db.create_tables([CLIENTS, ORDERS])
+
+    print('DONE')
+
+
+def test_db_info_clients():
+    all_clients = CLIENTS.select()
+    for client in all_clients:
+        if (client.name != ' ') and (client.city != ' ') and (client.address != ' '):
+            print("Succesfull")
+        else:
+            print("Error!")
+
+
+def test_db_info_orders():
+    all_orders = ORDERS.select()
+    for order in all_orders:
+        if (order.client != ' ') and (order.date != ' ') and (order.amount != ' ') and (order.description != ' '):
+            print("Succesfull")
+        else:
+            print("Error!")
+
+
+def test_db_count_clients():
+    all_clients = CLIENTS.select()
+    if (len(all_clients) < 10):
+        print("Little data")
+    else:
+        print("Succesfull")
+
+
+def test_db_count_clients():
+    all_orders = ORDERS.select()
+    if (len(all_orders) < 10):
+        print("Little data")
+    else:
+        print("Succesfull")
